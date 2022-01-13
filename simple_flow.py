9403f4f1d27088813446cfe257d08a1ac26c7d96
Yilun Guan <zoom.aaron@gmail.com>
2022-01-13 20:13:28 +0000

    add test smurf file processing capability

--- simple_flow.py
+++ simple_flow.py
@@ -1,10 +1,7 @@
 """This simple flow will aim to test watchdog"""
 
-import os.path as op, os
-import pickle
-from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
 from datetime import timedelta
-from prefect.schedules import IntervalSchedule
+
 import prefect
 from prefect import task, Flow
 from prefect.schedules import IntervalSchedule
@@ -13,11 +10,15 @@ WATCH_DIR = "/home/yguan/work/e2e_test/out/simset1"
 SNAP_PATH = ".watchdog"
 
 @task
-def get_new_files(watch_dir, snap_path=SNAP_PATH):
+def watch_files(watch_dir, snap_path=SNAP_PATH):
+    import pickle, os, os.path as op
+    from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
+
     logger = prefect.context.get("logger")
     dirname_transformer = lambda name: op.basename(name).replace("/","_").replace("-","_")
     snap = DirectorySnapshot(watch_dir, recursive=True)
     if not op.exists(snap_path): os.makedirs(snap_path)
+
     snap_file = op.join(snap_path, dirname_transformer(watch_dir))
     if op.exists(snap_file):
         logger.info(f"Found snapshot: {snap_file}")
@@ -35,9 +36,27 @@ def get_new_files(watch_dir, snap_path=SNAP_PATH):
         logger.info("Nothing else to do")
         return []
 
+@task
+def process_smurf(files):
+    from sotodlib.io.load_smurf import load_file
+    from sotodlib.tod_ops.fft_ops import calc_psd
+
+    logger = prefect.context.get("logger")
+    # find smurf files
+    smurf_files = [f for f in files if "timestreams" in f]
+    for smurf_file in smurf_files:
+        aman = load_file(smurf_file, load_primary=False, load_biases=False)
+        # do something to these data, such as calculating psd
+        freq, psd = calc_psd(aman)
+        logger.info(f"freq: {freq}")
+        logger.info(f"psd: {psd}")
+
+    return smurf_files
+
 schedule = IntervalSchedule(interval=timedelta(minutes=1))
 
 with Flow("file-watcher", schedule) as flow:
-    files = get_new_files(WATCH_DIR)
+    files = watch_files(WATCH_DIR)
+    process_smurf(files)
 
 flow.run()
