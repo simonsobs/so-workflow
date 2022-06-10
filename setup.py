eb3a90faf46c45ccd8f97fab09a88c9a9c6d6e9f
Yilun Guan <zoom.aaron@gmail.com>
2022-06-10 14:57:46 +0000

    package rename

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="so-workflow",
+    name="soworkman",
     version="0.1.0",
     packages=find_packages(where=".", exclude="tests"),
 )
